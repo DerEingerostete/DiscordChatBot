db6e93c47a43a7885b9e474f24fe2d92ee7ee343
DerEingerostete <dereingerostete@outlook.de>
2020-10-03 14:15:30 +0200

    Changed Core.java

--- src/main/java/bot/discord/Core.java
+++ src/main/java/bot/discord/Core.java
@@ -6,6 +6,10 @@ public class Core {
     private static final String TOKEN = "";
     private static final String PREFIX = "";
 
+    /**
+     * Run your discord bot
+     * @param args
+     */
     public static void main(String[] args) {
         try {
             Bot bot = new Bot(TOKEN, PREFIX);
